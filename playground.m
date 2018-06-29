92db9b1fd22a87e7adff8469b56e9a95a3942ffb
Zhicong <1984147727@qq.com>
2018-06-29 10:33:00 +0800

    add pics

---
+++ playground.m
@@ -0,0 +1,30 @@
+clear;clc;
+
+img = imread('data\origin6.png');
+% img = imresize(img, 0.25);
+figure;
+imshow(img);
+imgR = img(:, :, 1);
+imgG = img(:, :, 2);
+imgB = img(:, :, 3);
+% figure;
+% imshow(imgR);
+% figure;
+% imshow(imgG);
+% figure;
+% imshow(imgB);
+
+IMGR = fft2(imgR);
+GuvR = fftshift(IMGR);
+figure;
+imshow(log(abs(GuvR) + 1), []);
+
+IMGG = fft2(imgG);
+GuvG = fftshift(IMGG);
+figure;
+imshow(log(abs(GuvG) + 1), []);
+
+IMGB = fft2(imgB);
+GuvB = fftshift(IMGB);
+figure;
+imshow(log(abs(GuvB) + 1), []);
